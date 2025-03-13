7e6fb258330c70c11f45f909f892766863da002e
Michael Merrill <michael@michaelmerrill.me>
2025-03-13 17:43:20 -0600

    add drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import "@/utils/env";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
